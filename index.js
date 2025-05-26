561edbf9c77921ba4359baee91140b6f671502dc
Kaan Olmez <kaan.olmez@gmail.com>
2025-05-26 15:34:27 +0300

    cors fix2

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',              // test için tüm domainlere izin ver
+  methods: ['GET', 'POST'], // sadece gerekli methodlara izin ver
+  allowedHeaders: ['Content-Type']
+}));
 app.use(bodyParser.json());
 
 // MongoDB'ye bağlan
